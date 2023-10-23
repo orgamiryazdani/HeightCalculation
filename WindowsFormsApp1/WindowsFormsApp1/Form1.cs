18218bbc97a1521a7d6ac0060d9b8c748166039b
Amir Yazdani <100785957+orgamiryazdani@users.noreply.github.com>
2023-10-23 12:27:24 +0330

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count = 0; 
+        double sum = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (count < 20)
+                {
+                    int number = int.Parse(textBox1.Text);
+                    listBox1.Items.Add(number.ToString());
+                    textBox1.Clear();
+                    sum += number;
+                    count++;
+                    double average = sum / count;
+                    label1.Text = "میانگین : " + average.ToString();
+                }
+                else
+                {
+                    label1.Text = "تعداد مجاز قد وارد شده به 20 رسیده است.";
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+    }
+}
